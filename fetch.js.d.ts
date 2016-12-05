8c56ac8aa2231db1283a8e6d54e9e49d8bd845b6
Ali Khalili <ali.khll.1994@gmail.com>
2016-12-05 12:41:39 +0330

    Fetch Options declarations

--- fetch.js.d.ts
+++ fetch.js.d.ts
@@ -4,9 +4,16 @@
 // Definitions by: Ryan Graham <https://github.com/ryan-codingintrigue>
 
 interface FetchOptions {
-	method: string;
-	headers: any;
-	body: any;
+    method?: "GET" | "POST" | "DELETE" | "PATCH" | "PUT" | "HEAD" | "OPTIONS" | "CONNECT";
+    headers?: any;
+    body?: any;
+    mode?: "cors" | "no-cors" | "same-origin";
+    credentials?: "omit" | "same-origin" | "include";
+    cache?: "default" | "no-store" | "reload" | "no-cache" | "force-cache" | "only-if-cached";
+    redirect?: "follow" | "error" | "manual";
+    referrer?: string;
+    referrerPolicy?: "referrer" | "no-referrer-when-downgrade" | "origin" | "origin-when-cross-origin" | "unsafe-url";
+    integrity?: any;
 }
 
 declare enum ResponseType {
